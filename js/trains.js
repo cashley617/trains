c87896da3f9dced84f85bcb32a6c753ced2ac92d
Cate Ashley <catemarie.ashley@gmail.com>
2019-06-21 13:41:26 -0600

    Set up initial folders and firebase

---
+++ js/trains.js
@@ -0,0 +1,21 @@
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyC1e8541irxRhL8RsQE7bcGnGwbD2NioLo",
+    authDomain: "trains-cc214.firebaseapp.com",
+    databaseURL: "https://trains-cc214.firebaseio.com",
+    projectId: "trains-cc214",
+    storageBucket: "trains-cc214.appspot.com",
+    messagingSenderId: "368590457560",
+    appId: "1:368590457560:web:a8c89e6b40908d25"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
